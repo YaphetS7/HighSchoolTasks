f96428121b38226fe560f4441f07527f149d1837
YaphetS7 <interni17121999@mail.ru>
2019-01-20 22:52:41 +0400

    ...

---
+++ Queue/Qeue/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+namespace AlgorithmsDataStructures
+{
+
+    public class Queue<T>
+    {
+        private int count;
+        private LinkedList<T> list = new LinkedList<T>();
+        public Queue()
+        {
+            count = 0;
+        }
+
+        public void Enqueue(T item)
+        {
+            list.AddLast(item);
+            count++;
+        }
+
+        public T Dequeue()
+        {
+            if (count == 0)
+                return default(T);
+            else
+            {
+                T temp = list.First.Value;
+                list.RemoveFirst();
+                count--;
+                return temp;
+            }
+        }
+
+        public int Size()
+        {
+            return count;
+        }
+
+    }
+}
+
