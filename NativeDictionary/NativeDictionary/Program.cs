0167a787d3838321e279ded9780e16b2fbc6786c
YaphetS7 <interni17121999@mail.ru>
2019-01-27 22:26:21 +0400

    ...

--- NativeDictionary/NativeDictionary/Program.cs
+++ NativeDictionary/NativeDictionary/Program.cs
@@ -30,6 +30,8 @@ namespace AlgorithmsDataStructures
                 p_pow = p_pow * p;
             }
             sum += a[0] - '0';
+            if (sum < 0)
+                sum *= -1;
             return (sum % size);
         }
 
