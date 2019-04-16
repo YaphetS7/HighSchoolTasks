6e46dbcd636c241820eaa9c270fc67351c7ec66d
YaphetS7 <interni17121999@mail.ru>
2019-04-16 20:12:02 +0400

    16.04.2019

---
+++ ET_tree/ET_tree/EulerTourTree.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET_tree
+{
+    class EulerTourTree
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    /// <summary>
+    /// Класс, реализующий какое-либо ребро графа с направлением left-right.
+    /// </summary>
+    public class Edge
+    {
+        /// <summary>
+        /// Доступные для ребра вершины left и right.
+        /// </summary>
+        public Node left, right;
+
+        public bool hit = false;
+        public Edge()
+        {
+
+        }
+        public Edge(Node first, Node second)
+        {
+            left = first;
+            right = second;
+        }
+    }
+
+    /// <summary>
+    /// Класс, реализующий какую-либо вершину графа.
+    /// </summary>
+    public class Node
+    {
+        /// <summary>
+        /// Список ребер, связанных с текущей вершиной Node.
+        /// </summary>
+        public List<Edge> edges = new List<Edge>();
+
+        public Node parent, left, right;
+        public Node()
+        {
+            parent = null;
+            left = null;
+            right = null;
+        }
+    }
+
+    /// <summary>
+    /// Класс, содержащий отдельные Эйлеровы графы.
+    /// </summary>
+    public class Set
+    {
+        /// <summary>
+        /// Инициализирует новый граф с корнем node
+        /// </summary>
+        /// <param name="node"></param>
+        public Set(Node node)
+        {
+            tree.root = node;
+        }
+
+        /// <summary>
+        /// Проверка на принадлежность node к данному графу.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public bool Check(Node node)
+        {
+            return (tree.Find(node));
+        }
+
+        /// <summary>
+        /// Splay-tree with implicit keys, хранящее в себе Эйлеров обход графа.
+        /// </summary>
+        public Splay_Tree tree = new Splay_Tree();
+
+        /// <summary>
+        /// Изменение текущего корня в эйлеровом графе.
+        /// </summary>
+        /// <param name="newroot"></param>
+        public void NewRoot(Node newroot)
+        {
+            if (Check(newroot))
+                tree.Splay(newroot);
+        }
+
+        /// <summary>
+        /// Эйлеров обход графа.
+        /// </summary>
+        /// <returns></returns>
+        public List<Node> EulerTour()
+        {
+            return tree.InOrderSearch(tree.root, new List<Node>());
+        }
+    }
+    /// <summary>
+    /// Класс, реализующий дерево Эйлерового обхода.
+    /// </summary>
+    public class ETT
+    {
+        /// <summary>
+        /// Список эйлеровых графов в текущей системе.
+        /// </summary>
+        public List<Set> list = new List<Set>();
+
+        /// <summary>
+        /// Проверка принадлежности двух вершин одному графу.
+        /// </summary>
+        /// <returns></returns>
+        public bool Equivalence(Node first, Node second)
+        {
+            if (first == null || second == null)
+                return false;
+            return (FindParent(first) == FindParent(second));
+        }
+
+        /// <summary>
+        /// Метод добавления ребра между Эйлеровыми деревьями.
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        public void AddEdge(Node first, Node second)
+        {
+            if (FindParent(first) != FindParent(second))
+            {
+                return;
+            }
+            Edge edge1 = new Edge(first, second);
+            Edge edge2 = new Edge(second, first);
+            first.edges.Add(edge1);
+            first.edges.Add(edge2);
+            second.edges.Add(edge1);
+            second.edges.Add(edge2);
+
+            Set set1 = FindSet(first);
+            Set set2 = FindSet(second);
+            set1.tree.Merge(set2.tree);
+            list.Remove(set2);
+        }
+        private Set FindSet(Node node)
+        {
+            foreach (Set set in list)
+            {
+                if (set.Check(node))
+                    return set;
+            }
+            return null;
+        }
+        private Node FindParent(Node node)
+        {
+            while (node.parent != null)
+            {
+                node = node.parent;
+            }
+            return node;
+        }
+    }
+}
---
+++ ET_tree/ET_tree/Splay_tree.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ET_tree
+{
+    public class Splay_Tree
+    {
+        public Node root;
+        public Splay_Tree()
+        {
+            root = new Node();
+        }
+
+        /// <summary>
+        /// InOrderSearch для реализации Эйлерового обхода графа.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public List<Node> InOrderSearch(Node node, List<Node> list)
+        {
+            if (node == null)
+                return list;
+            InOrderSearch(node.left, list);
+            list.Add(node);
+            InOrderSearch(node.right, list);
+            return list;
+        }
+
+        /// <summary>
+        /// Метод, делающий node корнем.
+        /// </summary>
+        /// <param name="node"></param>
+        public void Splay(Node node)
+        {
+
+            if (node.parent == null)
+            {
+                root = node;
+                return;
+            }
+            Node parent = node.parent;
+            Node gparent = node.parent.parent;
+            if (gparent == null)
+            {
+                Rotate(parent, node);
+                root = node;
+                return;
+            }
+            else
+            {
+                bool zigzig = (gparent.left == parent) == (parent.left == node);
+                if (zigzig)
+                {
+                    Rotate(gparent, parent);
+                    Rotate(parent, node);
+                }
+                else
+                {
+                    Rotate(parent, node);
+                    Rotate(gparent, node);
+                }
+            }
+            Splay(node);
+        }
+        private void Rotate(Node parent, Node child)
+        {
+            Node gparent = parent.parent;
+            if (gparent != null)
+            {
+                if (gparent.left == parent)
+                    gparent.left = child;
+                else
+                    gparent.right = child;
+            }
+
+            if (parent.left == child)
+            {
+                parent.left = child.right;
+                child.right = parent;
+                parent.parent = child;
+                if (parent.left != null)
+                    parent.left.parent = parent;
+            }
+            else
+            {
+                parent.right = child.left;
+                parent.parent = child;
+                child.left = parent;
+                if (parent.right != null)
+                    parent.right.parent = parent;
+            }
+            child.parent = gparent;
+        }
+
+        /// <summary>
+        /// DFS для поиска вершины в дереве с implicit keys.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public bool Find(Node node)
+        {
+            Node current = root;
+            Queue<Node> que = new Queue<Node>();
+            que.Enqueue(current);
+            while (que.Count > 0)
+            {
+                if (current.left != null)
+                    que.Enqueue(current.left);
+                if (current.right != null)
+                    que.Enqueue(current.right);
+                current = que.Dequeue();
+                if (current == node)
+                    return true;
+            }
+            return false;
+        }
+        private Node FindMaxFrom(Node start)
+        {
+            while (start.right != null)
+                start = start.right;
+            return start;
+        }
+        private Node FindMinFrom(Node start)
+        {
+            while (start.left != null)
+                start = start.left;
+            return start;
+        }
+        public void Merge(Splay_Tree tree)
+        {
+            Node max = FindMaxFrom(root);
+            Node min = FindMinFrom(tree.root);
+            Splay(max);
+            max.right = tree.root;
+            tree.root.parent = max;
+        }
+    }
+}
